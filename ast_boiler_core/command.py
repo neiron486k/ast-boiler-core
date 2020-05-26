ec6b8933e7af888e056933e211b102b78b66f722
Alexey Astafev <efsneiron@gmail.com>
2020-05-26 16:45:21 +0300

    added ability to run web server into boiler command line

--- ast_boiler_core/command.py
+++ ast_boiler_core/command.py
@@ -12,7 +12,6 @@ def cli():
 def init():
     """Init project structure"""
     filename = 'src/__init__.py'
-
     os.makedirs(os.path.dirname(filename), exist_ok=True)
 
     if not os.path.exists(filename):
@@ -25,3 +24,10 @@ def init():
     if not os.path.exists('.env'):
         target = os.path.join(os.path.dirname(__file__), '.env.default')
         shutil.copy(target, '.env')
+
+
+@cli.command()
+def run():
+    """Run web server for development with auto reload"""
+    from src import app
+    app.run()
