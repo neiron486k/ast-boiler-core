2368748550d9cd0867e76f3eef983b94bbf529c7
Alexey Astafev <efsneiron@gmail.com>
2020-05-27 18:23:53 +0300

    removed redundant from tests at the moment

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,25 +1,6 @@
-import pytest
 from ast_boiler_core import create_app
-import tempfile
-import os
 
 
-@pytest.fixture
-def client():
-    app = create_app()
-    db_fd, app.config['DATABASE'] = tempfile.mkstemp()
-    app.config['TESTING'] = True
-
-    with app.test_client() as client:
-        with app.app_context():
-            # need to create database
-            pass
-
-        yield client
-
-    os.close(db_fd)
-    os.unlink(app.config['DATABASE'])
-
-
-def test_create_app(client):
+def test_create_app():
+    create_app()
     assert True
