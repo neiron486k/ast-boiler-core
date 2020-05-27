a356f9dae7a1caeef5dc08220e6a001db7a25a1e
Alexey Astafev <efsneiron@gmail.com>
2020-05-27 14:50:23 +0300

    added first test

---
+++ tests/test_core.py
@@ -0,0 +1,26 @@
+import pytest
+from ast_boiler_core import create_app
+import tempfile
+import os
+import unittest
+
+
+@pytest.fixture
+def client():
+    app = create_app()
+    db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+    app.config['TESTING'] = True
+
+    with app.test_client() as client:
+        with app.app_context():
+            # need to create database
+            pass
+
+        yield client
+
+    os.close(db_fd)
+    os.unlink(app.config['DATABASE'])
+
+
+def test_create_app(client):
+    assert True
